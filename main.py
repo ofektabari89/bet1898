a19ea81d8140ca442657b514ff776cab55f2cb12
ofektabari89 <ofektabari89@gmail.com>
2025-07-17 14:51:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("הבוט שלך מחובר!")
+    await update.message.reply_text("הבוט מחובר ועובד!")
 
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
+if __name__ == "__main__":
+    main()
