1eecfcc15bc7054c08f79819b4e5a2e84771b0ac
ofektabari89 <ofektabari89@gmail.com>
2025-07-17 14:45:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("הבוט שלך מחובר!")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
